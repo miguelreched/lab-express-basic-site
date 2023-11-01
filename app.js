9c8ccc41ab6250fa8b90d3551543f5fa955f7dda
Adrian Siquier <adriansiquiervalles@gmail.com>
2023-11-01 08:58:53 +0100

    lab done

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-const port = 3001
+const port = 3001;
 
-app.get("/home", (req, res)=>{
-    res.send("Welcome th everybody")
-})
+app.get("/", (req, res) => {});
 
-app.listen(port, ()=>{
-    
-})
+app.get("/home", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/works", (req, res) => {
+  res.sendFile(__dirname + "/views/works.html");
+});
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/gallery", (req, res) => {
+  res.sendFile(__dirname + "/views/gallery.html");
+});
+
+app.listen(port, () => {});
